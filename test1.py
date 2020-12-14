cde9d1c3557dee7b5a9bdb268cd00dac5a1d61f0
Jon <250518824@qq.com>
2020-12-14 19:08:09 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,22 @@
+department1 = 'Security'
+department2 = 'Python'
+depart1_m = 'cq_bomb'
+depart2_m = 'wzq'
+COURSE_FEES_SEC = 45678.213534
+COURSE_FEES_Python = 1238.51231
+
+# line1 = 'Department1 name:' '%-15s' %department1 + 'Manager:'  '%-20s' %depart1_m + 'COURSE FEES:' + '%-20.3f'%COURSE\
+# _FEES_SEC  +'THE END'
+# line2 = 'Department2 name:' '%-15s' %department2 + 'Manager:'  '%-20s' %depart2_m + 'COURSE FEES:' + '%-20.3f'%COURSE\
+# _FEES_Python + 'THE END'
+line1 = 'Department1 name:' '{0:15}'.format(department1) + 'Manager:'  '{0:20}'.format(depart1_m) + 'COURSE FEES:' +\
+        '{0:<20.2f}'.format(COURSE_FEES_SEC)  +'THE END'
+line2 = 'Department2 name:' '%-15s' %department2 + 'Manager:'  '%-20s' %depart2_m + 'COURSE FEES:' + \
+        '%-20.3f'%COURSE_FEES_Python + 'THE END'
+
+
+length = len(line1)
+print('='*length)
+print(line1)
+print(line2)
+print('='*length)
